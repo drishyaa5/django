da89a8712a4717c45bbc5e46d22790ef278acc53
Drishya <drishyaa005@gmail.com>
2024-09-20 13:56:59 +0545

    bnb

---
+++ bnbclone/bnb/app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const SearchFilters = () => {
+  return (
+    <div className='h-[64px] flex flex-row items-center justify-between border rounded-full'>
+        <div>
+            <div className='flex flex-row items-center justify-between'>
+                <div className='w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100'>
+                    <p className='text-xs font-semibold'>Where</p>
+                    <p className='text-sm'>Wanted location</p>
+                </div>
+
+                <div className='h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100'>
+                    <p className='text-xs font-semibold'>Check In</p>
+                    <p className='text-sm'>Add Dates</p>
+                </div>
+
+
+                <div className='h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100'>
+                    <p className='text-xs font-semibold'>Check Out</p>
+                    <p className='text-sm'>Add Dates</p>
+                </div>
+
+                <div className='h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100'>
+                    <p className='text-xs font-semibold'>Who</p>
+                    <p className='text-sm'>Add guests</p>
+                </div>
+            </div>
+        </div>
+        <div className='p-2'>
+        <div className='p-4 bg-airbnb rounded-full text-white'>
+        <svg viewBox="0 0 32 32"
+        style={{display:'block', fill:'none', height: '16px', width: '16px', stroke: 'currentColor', strokeWidth:4, overflow:'visible'}} 
+        aria-hidden="true" 
+        role="presentation" 
+        focusable="false">
+            <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9">
+            </path>
+        </svg>
+        </div>
+    </div>   
+    </div>
+  )
+}
+
+export default SearchFilters
---
+++ bnbclone/bnb/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        Django BNB
+
+        <h2 className="text-airbnb">Django and next rule</h2>
+      </main>
+      
+  );
+}
