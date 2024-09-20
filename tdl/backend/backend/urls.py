42c1573ab15a437677970db380cddb269b11c01e
Drishya <drishyaa005@gmail.com>
2024-09-20 11:21:49 +0545

    new django

--- tdl/backend/backend/urls.py
+++ tdl/backend/backend/urls.py
@@ -7,5 +7,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", createUserView.as_view(), name="register"),
-    papth("api/token/", TokenObtainPairView.as_view(), name="get_token")
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="get_token"),
+    path("api-auth/", include("rest_framework.urls")),
+
+
 ]
